0a404f3f5d91f40979ec6521de802a861c14b185
samundra shrestha <samundras41@gmail.com>
2025-05-23 00:47:18 +0545

    minor

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
-  base: '/odin-library/',
+  base: '/odin-library/', 
   plugins: [
     tailwindcss(),
   ],
